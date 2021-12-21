1c2da67b66b3d82f2aac17e7cf434d88a05b3f29
Luis Fernando <lfernandoce96@gmail.com>
2021-12-21 14:03:32 -0500

    Escructura Inicial del Portafolio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portafolio online del desarrollador Luis Fernando Cornejo Effio.">
+    <meta name="keywords" content="desarrollador front-end y backend">
+    <meta name="author" content="Luis Fernando Cornejo Effio">
+    <title>Luis Cornejo Effio | Desarrollador</title>
+    <link rel="shortcut icon" href="img/codigo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+    <nav class="menu">
+        <div class="foto">
+            <p><img src="img/luis.png" alt="Luis Cornejo Effio" class="foto"></p>
+        </div>
+        <div class="contenedor contenedor-menu">
+            <a href="#sobre-mi">Sobre mí</a>
+            <a href="#proyectos">Proyectos</a>
+            <a href="#conocimientos">Habilidades</a>
+            <a href="#contacto">Contacto</a>
+        </div>
+    </nav>
+</body>
+</html>
